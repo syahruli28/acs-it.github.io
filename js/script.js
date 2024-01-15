9467baeff16ec8dfea90b8dc8b44bc5dc3d994b5
syahruli28 <syahruli917@gmail.com>
2024-01-15 14:39:23 +0700

    upload 1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(window).on('scroll', function(){
+	if($(window).scrollTop()){
+		$('.header').addClass('white');
+	}
+	else {
+		$('.header').removeClass('white');
+	}
+})
+
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
